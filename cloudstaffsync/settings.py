ee3f12df3b7b99c6c5d1b465f5092346e9a1b0d0
Kotapavankalyanreddy <pavankalyanreddykota@gmail.com>
2025-04-10 18:40:22 +0530

    Fix: added WhiteNoise and collected static files for Render deployment

--- cloudstaffsync/settings.py
+++ cloudstaffsync/settings.py
@@ -95,9 +95,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'employees/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "employees/static")] 
+
+
 
 MEDIA_URL = f'https://{os.getenv("AWS_STORAGE_BUCKET_NAME", "cloudstaffsync")}.s3.amazonaws.com/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
